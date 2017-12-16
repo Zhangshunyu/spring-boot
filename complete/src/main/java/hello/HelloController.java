77ab9b026546fd522915f1ae487e07af186c8188
Zhangshunyu <zhangshunyu@huawei.com>
2017-12-16 20:27:10 +0800

    init

--- complete/src/main/java/hello/HelloController.java
+++ complete/src/main/java/hello/HelloController.java
@@ -1,5 +1,6 @@
 package hello;
 
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,7 +9,14 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "\nGreetings from Spring Boot!";
+    }
+
+    @RequestMapping("/test")
+    public Test test(
+        @RequestParam(value="name", defaultValue="a") String name,
+        @RequestParam(value = "id", defaultValue = "1") String id) {
+        return new Test(name, id);
     }
     
 }
